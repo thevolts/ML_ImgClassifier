7776ced383c83394c8d85f3b663c10a1c0738d63
Kumar Attangudi Perichiappan Perichappan <thevolts@gmail.com>
2019-09-01 15:02:23 +0800

    add another class/button
    updated the index.html and the js file to add another class/button

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function app() {
   document.getElementById('class-a').addEventListener('click', () => addExample(0));
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
   document.getElementById('class-c').addEventListener('click', () => addExample(2));
-  //document.getElementById('class-d').addEventListener('click', () => addExample(3));
+  document.getElementById('class-d').addEventListener('click', () => addExample(3));
 
   while (true) {
     if (classifier.getNumClasses() > 0) {
@@ -78,7 +78,7 @@ async function app() {
       // Get the most likely class and confidences from the classifier module.
       const result = await classifier.predictClass(activation);
 
-      const classes = ['A', 'B', 'C'];
+      const classes = ['A', 'B', 'C','D'];
       document.getElementById('console').innerText = `
         prediction: ${classes[result.classIndex]}\n
         probability: ${result.confidences[result.classIndex]}
@@ -89,8 +89,6 @@ async function app() {
   }
 }
 
-
-app();
 
 async function setupWebcam() {
   return new Promise((resolve, reject) => {
@@ -110,3 +108,5 @@ async function setupWebcam() {
     }
   });
 }
+
+app();
