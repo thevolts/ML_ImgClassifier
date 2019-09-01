b0b1ce2df9c78faa4fd76fbd144a28740c000120
Kumar Attangudi Perichiappan Perichappan <thevolts@gmail.com>
2019-09-01 14:40:42 +0800

    updated button 4

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ async function app() {
   document.getElementById('class-a').addEventListener('click', () => addExample(0));
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
   document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  //document.getElementById('class-d').addEventListener('click', () => addExample(3));
 
   while (true) {
     if (classifier.getNumClasses() > 0) {
